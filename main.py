41236275cbf016a37097e24cbefcaefeae1368fa
Dr He <sina.salahshour.32@outlook.com>
2021-07-16 00:57:22 +0430

    c003

--- main.py
+++ main.py
@@ -13,10 +13,15 @@ def cRectPos(obj,y):
 def pipedraw(pipe):
     screen.blit(pipe.value, pipe.rect)
     screen.blit(pipe.value2, pipe.rect2)
+def birddraw(bird):
+    screen.blit(pygame.transform.rotate(bird.value,bird.v*-10),bird.rect)
 pygame.init()
 bgsurface = object(pygame.image.load('assets/background-day.png'),[0,0])
 floor = object(pygame.image.load('assets/base.png'),[0,400])
 bird = object(pygame.image.load('assets/yellowbird-midflap.png'),[10,0])
+bird.value1 = pygame.image.load('assets/bluebird-midflap.png')
+bird.value2 = pygame.image.load('assets/yellowbird-downflap.png')
+bird.value3 = pygame.image.load('assets/yellowbird-upflap.png')
 bird.rect = bird.value.get_rect(center = (50,bgsurface.value.get_height()/2))
 pipe1 = object(pygame.image.load('assets/pipe-green.png'),[100, 200])
 pipe1.rect = pipe1.value.get_rect(center = (100,400))
@@ -48,7 +53,15 @@ while True:
     deltatime = clock.get_time()/5
     cRectPos(bird, 0.6 * 0.02 * (deltatime**2) + bird.v * deltatime)
     bird.v += gravity * deltatime/2
-    sdraw(bird)
+    if bird.v <0:
+        bird.value = bird.value2
+    elif bird.v >0:
+        bird.value = bird.value3
+    else:
+        bird.value = bird.value1
+    # sdraw(bird)
+    birddraw(bird)
+    print(bird.v)
     pipe1.rect.centerx -= 2
     pipe1.rect2.centerx = pipe1.rect.centerx
 
