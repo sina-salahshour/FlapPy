f99fe1f810946e0ac765d154aba73f335cd43b9c
Doctor He <sina.salahshour.32@gmail.com>
2021-07-16 11:21:26 +0000

    c006

--- main.py
+++ main.py
@@ -24,34 +24,57 @@ bird.value2 = pygame.image.load('assets/yellowbird-downflap.png')
 bird.value3 = pygame.image.load('assets/yellowbird-upflap.png')
 bird.rect = bird.value.get_rect(center = (50,bgsurface.value.get_height()/2))
 pipe1 = object(pygame.image.load('assets/pipe-green.png'),[100, 200])
-pipe1.rect = pipe1.value.get_rect(center = (100,400))
+pipe1.rect = pipe1.value.get_rect(center = (400,400))
 pipe1.value2 = pygame.transform.flip(pipe1.value, False, True)
-pipe1.rect2 = pipe1.value2.get_rect(center = (100,-15))
+pipe1.rect2 = pipe1.value2.get_rect(center = (400,-15))
+wingsound = pygame.mixer.Sound('sounds/sfx_wing.wav')
+hitsound = pygame.mixer.Sound('sounds/sfx_hit.wav')
+diesound = pygame.mixer.Sound('sounds/sfx_die.wav')
+pointsound = pygame.mixer.Sound('sounds/sfx_point.wav')
+swooshingsound = pygame.mixer.Sound('sounds/sfx_swooshing.wav')
 screen = pygame.display.set_mode((bgsurface.value.get_width(),bgsurface.value.get_height()))
 pygame.display.set_caption('FlapPy',)
 clock = pygame.time.Clock()
 bird.v=0
 gravity = 0.05
 pipe1.gap = 415
+dead = [0,0]
+betweenpipe = False
+dontmove = False
+def die(dead):
+    if dead[0] == 0:
+        hitsound.play()
+    dead[0]  = 1
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                bird.v = -1.5
-    if bird.rect.centery >=400:
-        exit()
-    if bird.rect.centerx+bird.value.get_width()/2 < pipe1.rect.centerx + pipe1.value.get_width()/2 and bird.rect.centerx-bird.value.get_width()/2 > pipe1.rect.centerx - pipe1.value.get_width()/2:
+            if dead[0] == 0:
+                if event.key == pygame.K_SPACE:
+                    bird.v = -1.5
+                    wingsound.play()
+    if bird.rect.centery >=375:
+        die(dead)
+        dontmove = True
+    if bird.rect.centerx-bird.value.get_width()/2 < pipe1.rect.centerx + pipe1.value.get_width()/2 and bird.rect.centerx+ bird.value.get_width()/2 > pipe1.rect.centerx - pipe1.value.get_width()/2:
         if bird.rect.centery + bird.value.get_height()/2 > pipe1.rect.centery - pipe1.value.get_height()/2 or bird.rect.centery - bird.value.get_height()/2 < pipe1.rect2.centery + pipe1.value2.get_height()/2:
-            exit()
+            die(dead)
+        else:
+            betweenpipe = True
+    else:
+        if betweenpipe:
+            pointsound.play()
+            betweenpipe = False
     sdraw(bgsurface)
-    floor.pos[0] -=2
+    if dead[0] == 0:
+        floor.pos[0] -=2
     if floor.pos[0] <= -floor.value.get_width():
         floor.pos[0] = 0
     deltatime = clock.get_time()/5
-    cRectPos(bird, 0.6 * 0.02 * (deltatime**2) + bird.v * deltatime)
+    if dontmove == False:
+        cRectPos(bird, 0.6 * 0.02 * (deltatime**2) + bird.v * deltatime)
     bird.v += gravity * deltatime/2
     if bird.v <0:
         bird.value = bird.value2
@@ -62,8 +85,16 @@ while True:
     # sdraw(bird)
     birddraw(bird)
     # print(bird.v)
-    pipe1.rect.centerx -= 2
-    pipe1.rect2.centerx = pipe1.rect.centerx
+    if dead[0] == 0:
+        pipe1.rect.centerx -= 2
+        pipe1.rect2.centerx = pipe1.rect.centerx
+
+    if dead[0] == 1:
+        dead[1] +=1
+        if dead[1] == 15:
+            diesound.play()
+        if dead[1] == 45:
+            exit()
 
     if pipe1.rect.centerx <= -pipe1.value.get_width()/2:
         pipe1.rect.centerx = bgsurface.value.get_width()+pipe1.value.get_width()/2
